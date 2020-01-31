968856edf2f6cedca2bf9d285dfa9fb49c284bb8
Arttu Kesanto <arttu.kesanto@gmail.com>
2020-02-01 01:05:51 +0200

    Bookstore work related to Haaga-Helia, course 'Palvelinohjelmointi'

---
+++ Bookstore/src/main/java/hh/swd/bookstore/Bookstore/domains/Book.java
@@ -0,0 +1,10 @@
+package hh.swd.bookstore.Bookstore.domains;
+
+public class Book {
+	private String title;
+	private String author;
+	private Integer year;
+	private String isbn;
+	private long price;
+
+}
---
+++ Bookstore/src/main/java/hh/swd/bookstore/Bookstore/webcontrollers/BookController.java
@@ -0,0 +1,21 @@
+package hh.swd.bookstore.Bookstore.webcontrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+
+@RequestMapping(value = "/index", method = RequestMethod.GET)
+public String someMethod(Model model) {
+	// Example code.
+	return "something";
+	
+	
+	
+}
+
+}
